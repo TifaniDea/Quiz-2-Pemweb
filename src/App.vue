29bf3a897391d33e84b022f27cc8373f860cd1d6
TifaniDea <tifanidea@students.amikom.ac.id>
2023-06-06 22:04:00 +0700

    Upload Quiz

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import navbar from './components/navbar.vue';
+import tampilan2 from './components/tampilan2.vue';
+import tampilan3 from './components/tampilan3.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <navbar />
+  </header>
+  <tampilan2 />
+  <tampilan3 />
+</template>
